e5a5aabd92673a8659bdf6e3586364236875dc6d
RahmaYasser <rahma.y9079@gmail.com>
2022-07-06 16:51:16 +0200

    new set

--- cut_wav.py
+++ cut_wav.py
@@ -3,23 +3,30 @@ from string import Template
 
 # times between which to extract the wave from
 start = 0 # seconds
-end = 180 # seconds
-# file to extract the snippet from
-with wave.open('/home/rahma/Downloads/SS149_Kassywedding.wav', "rb") as infile:
-    # get file data
-    nchannels = infile.getnchannels()
-    sampwidth = infile.getsampwidth()
-    framerate = infile.getframerate()
-    # set position in wave to start of segment
-    infile.setpos(int(start * framerate))
-    # extract data
-    data = infile.readframes(int((end - start) * framerate))
+end = 5 # seconds
+file_num=1
+while end <=180:
+    # file to extract the snippet from
+    with wave.open('/home/rahma/Downloads/SS149_Kassywedding.wav', "rb") as infile:
+        # get file data
+        nchannels = infile.getnchannels()
+        sampwidth = infile.getsampwidth()
+        framerate = infile.getframerate()
+        # set position in wave to start of segment
+        infile.setpos(int(start * framerate))
+        # extract data
+        data = infile.readframes(int((end - start) * framerate))
 
-# write the extracted data to a new file
-with wave.open('/home/rahma/speech_files/voice.wav', 'w') as outfile:
-    outfile.setnchannels(nchannels)
-    outfile.setsampwidth(sampwidth)
-    outfile.setframerate(framerate)
-    outfile.setnframes(int(len(data) / sampwidth))
-    outfile.writeframes(data)
-
+    # write the extracted data to a new file
+    st = str(file_num)
+    new = Template('/home/rahma/speech_files/$st.wav')
+    path_ = str(new.substitute(st= st))
+    with wave.open(path_, 'w') as outfile:
+        outfile.setnchannels(nchannels)
+        outfile.setsampwidth(sampwidth)
+        outfile.setframerate(framerate)
+        outfile.setnframes(int(len(data) / sampwidth))
+        outfile.writeframes(data)
+    start+=5
+    end+=5
+    file_num+=1
